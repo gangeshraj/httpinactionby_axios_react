2d92dd04c636985945724e78b13a90f64c7cb686
gangeshraj <gangeshraj1@gmail.com>
2018-04-19 08:12:06 +0530

    added comments in fullpost.js why componentDidMount and componentDidUpdate both used

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -9,17 +9,23 @@ class FullPost extends Component {
         loadedPost: null
     }
 
-    componentDidMount () {
+    componentDidMount () {//when component is mounted we are loading data as 
+        // fullpost component is nested routed component of posts.js
+        //it loads full post below the posts component
         console.log(this.props);
         this.loadData();
     }
 
-    componentDidUpdate() {
+    componentDidUpdate() {//keep in mind when we are in the same component which we already mounted 
+        //so it wont be again mounted and remounted therefore it only updates component
+        // and thus again in componentDidUpdate we should load data
         this.loadData();
     }
 
     loadData () {
         if ( this.props.match.params.id ) {
+            //adding + turns this into number which it is string here
+            //or else have != only value check
             if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id) ) {
                 axios.get( '/posts/' + this.props.match.params.id )
                     .then( response => {
